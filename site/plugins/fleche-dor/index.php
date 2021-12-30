23f2e0019b4dcd9d9774eb73efbb622b41df09a9
Sebastian Castro <sebastian.castro@protonmail.com>
2021-12-30 12:49:43 +0100

    Simplify filters admin

--- site/plugins/fleche-dor/index.php
+++ site/plugins/fleche-dor/index.php
@@ -1,29 +1,9 @@
 <?php
 Kirby::plugin('seb/flechedor', [
     'fieldMethods' => [
-        'toColors' => function($field) {
-            $names = array_map('trim', explode(',', $field->value));
-            $colors = [];
-            $filters = $field->model()->parent()->filters()->toStructure();
-            foreach($names as $name) {
-                foreach($filters as $filter) {
-                    if($filter->name() == $name) {
-                        $colors[] = $filter->color();
-                    }
-                }
-            }
-            $field->value = '<ul class="tags-colors">';
-            foreach($colors as $color) {
-                $field->value .= '<li style="background-color: '.$color.'"></li>';
-            }
-            $field->value .= '</ul>';
-            return $field;
-        },
-        'boolToStr' => function($field) {
-             return $field->isTrue() ? "oui" : "non";
+        'boolToStr' => function ($field) {
+            return $field->isTrue() ? "oui" : "non";
         }
     ],
-    'hooks' => [
-
-    ]
+    'hooks' => []
 ]);
